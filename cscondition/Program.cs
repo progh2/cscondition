8fe88c6518e898df709fadbca6a65e5e0406cbdb
Gihun Ham <progh2@gmail.com>
2020-05-05 23:54:29 +0900

    13-1. if 조건문 실습

--- cscondition/Program.cs
+++ cscondition/Program.cs
@@ -10,6 +10,17 @@ namespace cscondition
     {
         static void Main(string[] args)
         {
+            Console.Write("숫자 입력 :");
+            var input = int.Parse(Console.ReadLine());
+            
+            if(input % 2 == 0)
+            {
+                Console.WriteLine("짝수 입니다!");
+            }
+            if(input %2 == 1)
+            {
+                Console.WriteLine("홀수 입니다!");
+            }
         }
     }
 }
